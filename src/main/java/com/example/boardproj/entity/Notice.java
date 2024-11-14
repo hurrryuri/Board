4ab2b3d2538a01551c5a35aa0df487cdb1d392ff
aaaa <aaaa@a.a>
2024-11-14 17:33:23 +0900

    처음하는 커밋

---
+++ src/main/java/com/example/boardproj/entity/Notice.java
@@ -0,0 +1,13 @@
+package com.example.boardproj.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notice {
+}
